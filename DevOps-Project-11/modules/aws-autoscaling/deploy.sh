223d0fac68cdeedd57b822a81c4370a00ca049d4
sahithi <gullalasrisahithi@gmail.com>
2025-12-18 18:26:00 +0530

    Update deploy.sh

--- DevOps-Project-11/modules/aws-autoscaling/deploy.sh
+++ DevOps-Project-11/modules/aws-autoscaling/deploy.sh
@@ -9,7 +9,7 @@ apt install unzip
 unzip 2135_mini_finance.zip
 rm -rf 2135_mini_finance.zip index.nginx-debian.html
 cd 2135_mini_finance/
-mv * ../
+mv * ./
 rm -rf 2135_mini_finance/
 systemctl enable nginx
 systemctl restart nginx
