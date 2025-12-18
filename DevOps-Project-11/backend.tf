3a4b531e1e911a945352504fbf1a0c411b3aa623
sahithi <gullalasrisahithi@gmail.com>
2025-12-18 17:58:35 +0530

    Change S3 bucket name in backend configuration

--- DevOps-Project-11/backend.tf
+++ DevOps-Project-11/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "terra-rushie"
     region         = "us-east-1"
     key            = "DevOps Project-11/terraform.tfstate"
     dynamodb_table = "Lock-Files"
